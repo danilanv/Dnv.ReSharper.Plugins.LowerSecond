e29e62862965e31e097f5e84afcb6b3be5f6dbf1
Danila NV <@gmail.com>
2014-05-19 17:53:12 +0600

    - initial and final commit

---
+++ Dnv.ReSharper.Plugins.LowerSecond/Dnv.ReSharper.Plugins.LowerSecond.Tests/LowerSecondPluginTest.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Dnv.ReSharper.Plugins.LowerSecond.Tests
+{
+    [TestFixture]
+    public class LowerSecondPluginTests
+    {
+        private LowerSecondPlugin _target;
+        private List<string> _agr;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _target = new LowerSecondPlugin();
+            _agr = new List<string>();
+        }
+
+        [Test]
+        public void EvaluateQuickResult_ZeroArgument()
+        {
+            //act
+            var result = _target.EvaluateQuickResult(null, _agr);
+
+            //assert
+            Assert.That(string.IsNullOrEmpty(result), Is.True);
+        }
+
+        [Test]
+        public void EvaluateQuickResult_OneArgumentOneChar()
+        {
+            //arrange
+            _agr.Add("I");
+
+            //act
+            var result = _target.EvaluateQuickResult(null, _agr);
+
+            //assert
+            Assert.That(result, Is.EqualTo("_i"));
+        }
+
+        [Test]
+        public void EvaluateQuickResult_OneArgumentTwoChars()
+        {
+            //arrange
+            _agr.Add("IM");
+
+            //act
+            var result = _target.EvaluateQuickResult(null, _agr);
+
+            //assert
+            Assert.That(result, Is.EqualTo("_m"));
+        }
+
+        [Test]
+        public void EvaluateQuickResult_OneArgumentMoreThenTwoChars()
+        {
+            //arrange
+            _agr.Add("IBo");
+
+            //act
+            var result = _target.EvaluateQuickResult(null, _agr);
+
+            //assert
+            Assert.That(result, Is.EqualTo("_bo"));
+        }
+
+        [Test]
+        public void EvaluateQuickResult_MoreThenOneArguments_EmptyString()
+        {
+            //arrange
+            _agr.Add("IBo");
+            _agr.Add("Noko");
+
+            //act
+            var result = _target.EvaluateQuickResult(null, _agr);
+
+            //assert
+            Assert.That(string.IsNullOrEmpty(result), Is.True);
+        }
+    }
+}
---
+++ Dnv.ReSharper.Plugins.LowerSecond/Dnv.ReSharper.Plugins.LowerSecond/LowerSecondPlugin.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using JetBrains.DocumentModel;
+using JetBrains.ReSharper.Feature.Services.LiveTemplates.Hotspots;
+using JetBrains.ReSharper.Feature.Services.LiveTemplates.Macros;
+
+namespace Dnv.ReSharper.Plugins.LowerSecond
+{
+    [Macro("CustomTemplateMacros.LowerSecondPlugin",
+        ShortDescription = "Value of {#0:another variable} with the first character replaced with '_' and second character in lower case.",
+        LongDescription = "Value of another variable with the first character replaced with '_' and second character in lower case. Example: IApplicationSettings => _applicationSettings")]
+    public class LowerSecondPlugin : IMacro
+    {
+        private readonly ParameterInfo[] _parameters = new ParameterInfo[1] { new ParameterInfo(ParameterType.VariableReference) };
+
+        public LowerSecondPlugin()
+        {
+            _parameters = new ParameterInfo[1] { new ParameterInfo(ParameterType.VariableReference) { } };
+        }
+
+
+        public string GetPlaceholder(IDocument document)
+        {
+            return "_a";
+        }
+
+        public bool HandleExpansion(IHotspotContext context, IList<string> arguments)
+        {
+            return false;
+        }
+
+        public HotspotItems GetLookupItems(IHotspotContext context, IList<string> arguments)
+        {
+            return null;
+        }
+
+        public string EvaluateQuickResult(IHotspotContext context, IList<string> arguments)
+        {
+            if (arguments.Count == 1)
+            {
+                var arg = arguments[0];
+
+                if (arg.Length == 1)
+                    return "_" + arg[0].ToString().ToLower();
+
+                if (arg.Length >= 2)
+                {
+                    var result = "_" + arg[1].ToString().ToLower();
+                    if (arg.Length > 2)
+                        result += arg.Substring(2);
+
+                    return result;
+                }
+            }
+
+            return "";
+        }
+
+        public ParameterInfo[] Parameters
+        {
+            get { return _parameters; }
+        }
+    }
+}
